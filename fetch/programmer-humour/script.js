d89c16854363572e1b9491be955f427e041b18f7
Azar <azaralampanah2@gmail.com>
2025-01-06 14:09:15 +0000

    complete javascript codes to fetch image and show it

--- fetch/programmer-humour/script.js
+++ fetch/programmer-humour/script.js
@@ -1,3 +1,11 @@
 const fetchUrl="https://xkcd.now.sh/?comic=latest"
-fetch(fetchUrl).then(response=>response.json)
-.then(data=>console.log(data))
+fetch(fetchUrl).then(response=>response.json())
+.then(data=>{
+    const image=document.getElementById("imageId")
+    console.log(data)
+    image.src=data.img
+    
+})
+    
+.catch(err=>console.log(err))
+
