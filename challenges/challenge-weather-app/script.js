2f5d58d219cd2ada92f7d8e99dc93e50417f9c9e
Azar <azaralampanah2@gmail.com>
2025-01-18 11:53:53 +0000

    making everything work

---
+++ challenges/challenge-weather-app/script.js
@@ -0,0 +1,104 @@
+let currentTemp=document.getElementById("temp")
+let goBtn=document.getElementById("goBtn")
+let tempreture=document.getElementById("tempreture")
+let inputCity=document.getElementById("search-tf")
+let photo=document.getElementById("photo")
+let im=document.getElementById("im")
+let largePic=document.getElementById("largePic")
+let thumbNails=document.getElementById("thumbs")
+
+const state={
+ tempreture:"",
+ searchTerm:"",
+ images:[],
+ imageArr:[],
+ corresImageName:""
+}
+ 
+ function fetchingTemp(){
+  
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${state.searchTerm}&APPID=f0c663a41414fed40e10a4bdd0884f1e`)
+          .then(response=>
+                
+            
+            response.json()
+            )
+          .then(data=>{
+            console.log(data)
+           state.tempreture=((data.main.temp)-273.15).toFixed(2)
+       state.corresImageName=data.weather[0].description
+       console.log(state.corresImageName)
+        
+
+          tempreture.textContent=`current tempreture for ${state.searchTerm} is : ${state.tempreture} Celsius`
+          fetchImage(state.corresImageName)
+          
+          
+      }
+     
+      )
+ }
+ inputCity.addEventListener("keyup",()=>{
+thumbNails.innerHTML=""
+tempreture.textContent=""
+photo.style.backgroundImage = "none"
+           state.imageArr=[] 
+           state.images=[]
+  state.searchTerm=inputCity.value
+  
+  console.log(state.searchTerm)
+  
+})
+
+          
+        goBtn.addEventListener("click",(event)=>{
+          event.preventDefault()
+          //state.imageArr=[] 
+   // state.images=[]
+   photo.style.backgroundImage=""
+    thumbNails.innerHTML=""
+
+          fetchingTemp()
+          
+          
+    })
+   
+ 
+ function fetchImage(){     
+ fetch(`https://api.unsplash.com/search/photos?query=${state.corresImageName}&client_id=BGjI5c4diDleXiEuPRFmLiP1k-iJiQmSMtAPjb_3NKk`)
+ .then(response=>response.json())
+ .then(data=>{console.log(data.results)
+ state.images=data.results
+ nextfu(state.images)
+ 
+ })
+}
+function nextfu(){
+console.log(state.images)
+
+ state.images.forEach((imagess,index)=>{
+  
+  state.imageArr.push(imagess.links.download)
+  console.log(state.imageArr)
+  const img = document.createElement("img")
+  img.src=imagess.links.download
+  thumbNails.appendChild(img)
+  photo.classList.add('loaded');
+  img.addEventListener("click",()=>{
+    
+    
+    
+    photo.style.backgroundImage = `url(${state.imageArr[index]})`; // Set background image
+    
+   photo.style.backgroundSize = "contain"; // Optional: Adjust the background size
+   
+        console.log(state.imageArr)
+    
+  })
+  
+ }
+ 
+ )
+}
+
+ 
